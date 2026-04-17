19db57a4c2b410f9537af109567dbd1970577c0f
Joshua Packer <jpacker@redhat.com>
2026-04-17 11:20:19 -0400

    Fix runtime issue in dashboard
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- swarmer/config.py
+++ swarmer/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     agent_image_pull_secret: str = ""
     k8s_namespace: str = ""
 
-    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 settings = Settings()
